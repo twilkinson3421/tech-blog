38358d810edb923ee3095bbd49b699645ba1e4fb
Tom Wilkinson <twilkinson3421@outlook.com>
2021-09-01 21:17:52 +0100

    0109202102

--- src/components/OverviewArticles.js
+++ src/components/OverviewArticles.js
@@ -4,8 +4,30 @@ import classes from './OverviewArticles.module.css';
 
 function OverviewArticles() {
   return (
-    <section>
-      {/* Figure out how to display the data coming from the .json file... */}
+    <section className={classes.overviewArticlesSection}>
+      {overviewArticles.map((articleovview) => {
+        return (
+          <div key={articleovview.id} className={classes.ovvarticleSingle}>
+            <img
+              src={articleovview.url}
+              className={classes.ovvarticleImage}
+              alt={'THUMBNAIL_' + articleovview.id}
+            />
+            <div className={classes.ovvarticleAllText}>
+              <div className={classes.ovvarticleTopText}>
+                <p>{articleovview.date}</p>
+                <h2 className={classes.ovvarticleTitle}>
+                  {articleovview.title}
+                </h2>
+              </div>
+              <p>{articleovview.description}</p>
+            </div>
+            <div className={classes.ovvarticleReadMore}>
+              <p>Read More</p>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 }
